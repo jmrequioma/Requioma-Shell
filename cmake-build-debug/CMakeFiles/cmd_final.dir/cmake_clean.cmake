337563d0a8607129ac4e9fd4aec3dc59d732c3e1
jmrequioma <jerome.requioma@gmail.com>
2018-12-04 11:58:12 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/cmd_final.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmd_final.dir/main.c.obj"
+  "cmd_final.pdb"
+  "cmd_final.exe"
+  "cmd_final.exe.manifest"
+  "libcmd_final.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmd_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
